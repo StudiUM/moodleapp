f1c857fdd8a8412f19fc8b809ff7bb6b890f05cd
Issam Taboubi <issam.taboubi@umontreal.ca>
2024-10-08 14:18:23 -0400

    EVOSTDM-3459 - Appliquer la fonctionnalité de Marks per sections

--- src/addons/mod/quiz/services/quiz.ts
+++ src/addons/mod/quiz/services/quiz.ts
@@ -2127,6 +2127,7 @@ export type AddonModQuizAttemptWSData = {
         grade: number; // The grade this attempt earned for this item.
         maxgrade: number; // The total this grade is out of.
     }[];
+    attemptsectionsmarks?: string;
 };
 
 /**
