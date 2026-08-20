52f8ea9e10c3c4313ac0f3d133f083185b3db51b
Issam Taboubi <issam.tab@gmail.com>
2026-08-20 09:09:36 -0400

    MDLUM-9328 - Mettre à jour l'app mobile avec API level 36 (octobre 31)
    * MDLUM-9328 - Mettre à jour l'app mobile avec API level 36 (octobre 31)
    
    * MDLUM-9328 - Optimiser le deploiment sur les environnements StudiUM
    
    (cherry picked from commit dcf52e5a0b28da64708c074abadda4dbaefae463)

--- scripts/studiumios.sh
+++ scripts/studiumios.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
 FICHIER_XCCONFIG_DEBUG="$(pwd)/platforms/ios/cordova/build-debug.xcconfig"
-FICHIER_XCCONFIG_RELEASE="$(pwd)/platforms/ios/cordova/build-debug.xcconfig"
-LIGNE_A_AJOUTER="SWIFT_OBJC_INTERFACE_HEADER_NAME = Moodle-Swift.h"
+FICHIER_XCCONFIG_RELEASE="$(pwd)/platforms/ios/cordova/build-release.xcconfig"
 
 echo "Le répertoire courant est : $(pwd)"
 
-cat $FICHIER_XCCONFIG_DEBUG
-cat $FICHIER_XCCONFIG_RELEASE
-
-# Vérifier si ios
-if [ "$DEVICE_TARGET" != "ios" ]; then
-  echo "Rien à faire pour ios"
-  exit 1
+if [ -f "$FICHIER_XCCONFIG_DEBUG" ]; then
+  cat "$FICHIER_XCCONFIG_DEBUG"
+else
+  echo "Info: fichier absent $FICHIER_XCCONFIG_DEBUG"
 fi
 
-# if [ "$DEVICE_TARGET" == "ios" ]; then
-#   echo "Ajout de ligne $LIGNE_A_AJOUTER dans le debug"
-#   echo "$LIGNE_A_AJOUTER" >> "$FICHIER_XCCONFIG_DEBUG"
-#   echo "Ajout de ligne $LIGNE_A_AJOUTER dans le release"
-#   echo "$LIGNE_A_AJOUTER" >> "$FICHIER_XCCONFIG_RELEASE"
-# fi
+if [ -f "$FICHIER_XCCONFIG_RELEASE" ]; then
+  cat "$FICHIER_XCCONFIG_RELEASE"
+else
+  echo "Info: fichier absent $FICHIER_XCCONFIG_RELEASE"
+fi
+
+# Vérifier si ios
+TARGET="${DEVICE_TARGET:-${APPFLOW_BUILD_TARGET:-ios}}"
+TARGET_LC="$(printf '%s' "$TARGET" | tr '[:upper:]' '[:lower:]')"
+
+if [ "$TARGET_LC" != "ios" ]; then
+  echo "Rien à faire pour ios"
+  exit 0
+fi
 
 echo "Ici remplacement chaine"
 
-sed -i '' 's/#import "Moodle-Swift.h"/#import "StudiUM_mobile-Swift.h"/g' "$(pwd)/platforms/ios/StudiUM mobile/Plugins/@moodlehq/phonegap-plugin-push/EncryptionHandler.m"
-cat "$(pwd)/platforms/ios/StudiUM mobile/Plugins/@moodlehq/phonegap-plugin-push/EncryptionHandler.m"
+ENCRYPTION_HANDLER_PATH="$(pwd)/platforms/ios/StudiUM mobile/Plugins/@moodlehq/phonegap-plugin-push/EncryptionHandler.m"
+
+if [ ! -f "$ENCRYPTION_HANDLER_PATH" ]; then
+  echo "Info: fichier absent $ENCRYPTION_HANDLER_PATH"
+  exit 0
+fi
+
+if sed --version >/dev/null 2>&1; then
+  sed -i 's/#import "Moodle-Swift.h"/#import "StudiUM_mobile-Swift.h"/g' "$ENCRYPTION_HANDLER_PATH"
+else
+  sed -i '' 's/#import "Moodle-Swift.h"/#import "StudiUM_mobile-Swift.h"/g' "$ENCRYPTION_HANDLER_PATH"
+fi
+
+cat "$ENCRYPTION_HANDLER_PATH"
